64f2fb9beccc2f7332c9c68c7452d1ef81e6cdda
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-06-08 22:55:42 +0000

    Redis

--- utils/redis.js
+++ utils/redis.js
@@ -6,12 +6,16 @@ class RedisClient {
     this.client = redis.createClient();
     this.getAsync = promisify(this.client.get).bind(this.client);
 
-    this.client.on('connect', function() {
+    //this.client.on('connect', function() {
       //console.log('Connected server');
-    });
+    //});
 
     this.client.on('error', function(error) {
       console.error(error);
+    });
+
+    this.client.on('connect', function() {
+      //console.log('Connected server');
     });
   }
 
@@ -24,7 +28,8 @@ class RedisClient {
   }
 
   async get(key) {
-    return this.getAsync(key);
+    const val = this.getAsync(key);
+    return val;
   }
   async set(key, value, time) {
     this.client.setex(key, time, value);
