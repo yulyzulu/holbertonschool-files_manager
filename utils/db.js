5d6b77a54bbd2a056ebaec96eb1e7531879a4679
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-06-09 12:02:06 -0500

    MongoDB

--- utils/db.js
+++ utils/db.js
@@ -5,30 +5,24 @@ const PORT = process.env.DB_PORT || 27017;
 const DATABASE = process.env.DB_DATABASE || 'files_manager';
 const URI = `mongodb://${HOST}:${PORT}`;
 
-
 class DBClient {
   constructor() {
-    //const HOST = process.env.DB_HOST || 'localhost';
-    //const PORT = process.env.DB_PORT || 27017;
-    //const DATABASE = process.env.DB_DATABASE || 'files_manager';
-    //const URI = `mongodb://${HOST}:${PORT}`;
-
-    //this.client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-    MongoClient.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
+    MongoClient.connect(URI, { useUnifiedTopology: true }, (err, client) => {
       if (!err) {
         this.db = client.db(DATABASE);
         this.usersCollection = this.db.collection('users');
         this.filesCollection = this.db.collection('files');
       } else {
         console.log(err);
-        //this.db = false;
       }
     });
   }
 
   isAlive() {
-    return !!this.db;
+    if (this.db) {
+      return true;
+    }
+    return false;
   }
 
   async nbUsers() {
